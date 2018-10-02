53f9384988a6326a13b2bd17b35ec39b257de051
Dhayanandhan <dhayaec@gmail.com>
2018-10-02 22:33:30 +0530

    replace cra with next

---
+++ packages/web/server/index.ts
@@ -0,0 +1,29 @@
+import {createServer} from 'http';
+import * as next from 'next';
+import {parse} from 'url';
+
+const port = parseInt(process.env.PORT as string, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const url: any = req.url;
+    const parsedUrl = parse(url, true);
+    const {pathname, query} = parsedUrl;
+
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query);
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(port, (err: any) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
