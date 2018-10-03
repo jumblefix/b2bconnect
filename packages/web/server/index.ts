e2af687dd4ec87b1c98f9dc89148c39306b97f7d
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-10-03 19:37:14 +0530

    add jest for web app

--- packages/web/server/index.ts
+++ packages/web/server/index.ts
@@ -1,17 +1,17 @@
-import {createServer} from 'http';
+import { createServer } from 'http';
 import * as next from 'next';
-import {parse} from 'url';
+import { parse } from 'url';
 
 const port = parseInt(process.env.PORT as string, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev});
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const url: any = req.url;
     const parsedUrl = parse(url, true);
-    const {pathname, query} = parsedUrl;
+    const { pathname, query } = parsedUrl;
 
     if (pathname === '/a') {
       app.render(req, res, '/a', query);
