587bdaf1d9c80d9ad94723678daa360ba122e1c8
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-10-03 19:57:36 +0530

    add styled components

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,17 +1,32 @@
 import Link from 'next/link';
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const Button = styled.button`
+  color: red;
+`;
 
 export default () => (
-  <ul>
-    <li>
-      <Link href="/a" as="/a">
-        <a>a</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/b" as="/b">
-        <a>b</a>
-      </Link>
-    </li>
-  </ul>
+  <div>
+    <Wrapper>
+      <ul>
+        <li>
+          <Link href="/a" as="/a">
+            <a>a</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/b" as="/b">
+            <a>b</a>
+          </Link>
+        </li>
+      </ul>
+    </Wrapper>
+    <Button>Button</Button>
+  </div>
 );
