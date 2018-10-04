f756123c9838526a6f3a897004bbb5dc116a3860
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-10-04 18:54:35 +0530

    add root-static-files

--- packages/web/server/index.ts
+++ packages/web/server/index.ts
@@ -1,6 +1,7 @@
 import { createServer } from 'http';
 import * as next from 'next';
 import { parse } from 'url';
+const { join } = require('path');
 
 const port = parseInt(process.env.PORT as string, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -13,7 +14,12 @@ app.prepare().then(() => {
     const parsedUrl = parse(url, true);
     const { pathname, query } = parsedUrl;
 
-    if (pathname === '/a') {
+    const rootStaticFiles = ['/robots.txt', '/sitemap.xml', '/favicon.ico'];
+
+    if (rootStaticFiles.indexOf(pathname || '') > -1) {
+      const path = join(__dirname, 'static', pathname);
+      app.serveStatic(req, res, path);
+    } else if (pathname === '/a') {
       app.render(req, res, '/a', query);
     } else if (pathname === '/b') {
       app.render(req, res, '/b', query);
